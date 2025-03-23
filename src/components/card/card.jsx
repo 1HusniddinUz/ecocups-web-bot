cd847547f5df71a0a3f74a5e62c0df554dd0ffd3
husnidd1n.uz <husniddinhalimov112@gmail.com>
2025-03-23 20:20:06 +0500

    api

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,25 +2,32 @@ import React, { useState } from "react";
 import "./card.css";
 import Button from "../button/button";
 const Card = (props) => {
-  const [count, setCount] = useState(0)
-  const { cup, onAddItem , onRemoveItem } = props;
-  const handleIncrement = () =>{
-    setCount(prev => prev + 1 )
-    onAddItem(cup)
-  }
-  const handleDecrement = () =>{
-    setCount(prev => prev - 1 )
-    onRemoveItem(cup)
-  }
+  const [count, setCount] = useState(0);
+  const { cup, onAddItem, onRemoveItem } = props;
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+    onAddItem(cup);
+  };
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+    onRemoveItem(cup);
+  };
   return (
     <div className="card">
-      <span className={`${count !== 0 ? 'card_badge' : "card_badge-hidden"}`}>{count}</span>
+      <span className={`${count !== 0 ? "card_badge" : "card_badge-hidden"}`}>
+        {count}
+      </span>
       <div className="image_container">
         <img src={cup.Image} alt={cup.title} width={"100%"} height={"420px"} />
       </div>
       <div className="card_body">
         <h2 className="card_title">{cup.title}</h2>
-        <div className="card_price">$ {cup.price}</div>
+        <div className="card_price">
+          {cup.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </div>
       </div>
       <div className="hr"></div>
       <div className="btn_container">
